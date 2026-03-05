a0f86692f1624da4f5538d06a5c76e6f481c513c
mc-coi <zmccoy@gmail.com>
2026-03-05 11:40:58 -0500

    setup firebase-config

--- firebase-config.js
+++ firebase-config.js
@@ -6,13 +6,16 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTs2Ia-PN1mCaQHQyotGA9oFq-8_vE1Uk",
+  authDomain: "p2mt-8d513.firebaseapp.com",
+  projectId: "p2mt-8d513",
+  storageBucket: "p2mt-8d513.firebasestorage.app",
+  messagingSenderId: "981918337161",
+  appId: "1:981918337161:web:9de337082e62b24a51de58",
+  measurementId: "G-4H77EB2JND"
 };
 
 export const app = initializeApp(firebaseConfig);
